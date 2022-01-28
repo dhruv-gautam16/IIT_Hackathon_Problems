97af248e77decaebdc69bd5c114d257e7360cca9
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-01-28 09:12:49 +0530

    Solution 3

--- 2.c
+++ 2.c
@@ -1,50 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main()
 {
-    printf("Enter n \n");
-    int n;
-    scanf("%d", &n);
-    char s[n];
-    printf("\nenter string \n");
-    scanf("\n%[^\n]", s);
-    char ans='y';
-    printf("\nstring: %s",s);
-    int counter = 0;
-    while(ans == 'y'){
-
-        char pat[n];
-        printf("\nenter pattern ");
-        scanf("\n%[^\n]", pat);
-        printf("\npattern: %s", pat);
-       
-        int i, j, l1, l2;
-        int counter = 0;
-
-        l1 = strlen(s);
-        l2 = strlen(pat);
-
-        for(i = 0; i < l1 - 1; i++) {
-            if(strstr(s + i, pat) == s + i) {
-                printf("\nposition: %d", i+1);
-                counter++;
-                i = i + l2 -1; //to cover up the pattern length for next checking
-            }
+    int a,b,c,e,f,g,r,d;
+    while(scanf("%d %d %d %d", &a,&b,&c,&e) != -1)
+    {
+    f=c-b;
+    g=e-c;
+        if (f==0 && g==0)
+        {
+            r=1;
         }
-       
-        if(counter!=0)
-            printf("\noccurence of %s is:  %d ", pat, counter);
         else
-            printf("-1");
-           
-        printf("\ndo you want to check more patterns?y/n \n");
-        char choice;
-       
-        scanf("\n%c", &choice);
-        if(choice == 'n'){
-            ans=choice;
+        {
+            r=g/f;
+            
         }
+    d=b-a*r;
+    printf("%d %d %d\n",a,r,d);
     }
+    return 0;
 }
